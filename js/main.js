252580dfd53e0492afea86cd43f715e01674be30
M-S0212 <inteco13579@gmail.com>
2021-03-24 15:33:48 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-// document는 html 돋보기 눌러도 늘어나게 만드는 부분
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-
-  //Logic...
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  // searchInputEl에다가 html 속성 지정
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  // searchInputEl에다가 html 속성 지정
-  searchInputEl.setAttribute('placeholder', '');
-});
 
 //스크롤 내리면 뱃지 사라지게 하기
 const badgeEl = document.querySelector('header .badges');
@@ -185,7 +164,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-//몇년도인지 자동 계산
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //2021
